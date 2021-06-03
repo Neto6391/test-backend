79c280bccd60de18c4598d4b1f9ba7e65f3d08c1
nettorodrigues <neto6391@gmail.com>
2021-06-03 03:16:12 -0300

    create rotues of login and create user

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards, Request, Body } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { UserDto } from './dto/user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(
+        private userService: UserService
+    ) {}
+
+    @UseGuards(AuthGuard('local'))
+    @Post('authenticate')
+    async login(@Request() req) {
+        return await this.userService.authenticate(req.user);
+    }
+
+    @Post('create')
+    async signUp(@Body() user: UserDto) {
+        return await this.userService.create(user);
+    }
+
+}
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { USER_REPOSITORY } from 'src/core/constants';
+import { AuthService } from '../auth/auth.service';
+import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @Inject(USER_REPOSITORY) private readonly userRepository: typeof User,      
+        @Inject(forwardRef(() => AuthService)) private readonly authService: AuthService,
+    ) {}
+
+    async findOneByEmail(email: string): Promise<User> {
+        return await this.userRepository.findOne<User>({ where: { email } });
+    }
+
+    async findOneById(id: number): Promise<User> {
+        return await this.userRepository.findByPk<User>(id);
+    }
+
+    async authenticate(user) {
+        return await this.authService.login(user);
+    }
+
+    async create(user) {
+        const pass = await this.hashPassword(user.password);
+
+        const newUser = await this.userRepository.create({ ...user, password: pass });
+        const { password, ...result } = newUser['dataValues'];
+        
+        const token = await this.authService.generateToken(result);
+
+        return { user: result, token };
+    }
+
+    private async hashPassword(password) {
+        const hash = await bcrypt.hash(password, 10);
+        return hash;
+    }
+}
