e150ce0060adb1a6c58b9deef2c5ca5bdc99717e
nettorodrigues <neto6391@gmail.com>
2021-06-03 20:52:12 -0300

    create Create and Read for Person

--- src/modules/person/person.controller.ts
+++ src/modules/person/person.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Request, InternalServerErrorException, BadRequestException, Get } from '@nestjs/common';
+import { DoesPersonExist } from 'src/core/guards/does-person-exists.guard';
 import { JwtAuthGuard } from 'src/core/guards/jwt-auth.guard';
+import { PersonDto } from './dto/person.dto';
 import { PersonService } from './person.service';
 
 @Controller('person')
@@ -8,10 +10,47 @@ export class PersonController {
         private readonly personService: PersonService
     ) {}
 
-    @UseGuards(JwtAuthGuard)
+    @UseGuards(JwtAuthGuard, DoesPersonExist)
     @Post('create')
-    async create(@Body() body, @Request() req) {
-        
+    async create(@Body() body: PersonDto, @Request() req) {
+        const { id: userId } = req.user;
+        this.personService.create(body, userId);
+        return { message: "Person was successfully created!" };
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('read')
+    async read(@Request() req) {
+        let page;
+        let limit;
+        let name;
+    
+        const typeofVariableForbiddenForString = ["string","boolean", "object", "undefined"];
+
+        if (req.query.page && typeofVariableForbiddenForString.includes(typeof req.query.page)) {
+            if (req.query.page.length > 1) throw new BadRequestException('Bad Params in Body of Request');            
+        }
+
+        if (req.query.limit && typeofVariableForbiddenForString.includes(typeof req.query.limit)) {
+            if (req.query.limit.length > 1) throw new BadRequestException('Bad Params in Body of Request'); 
+        }
+
+        if (req.query.name && typeofVariableForbiddenForString.includes(typeof req.query.name)) {
+            if (req.query.name <= 1)  throw new BadRequestException('Bad Params in Body of Request'); 
+           
+        }
+
+        if (req.query.page) {
+            page = req.query.page;
+        }
+        if (req.query.limit) {
+            limit = req.query.limit;
+        }
+        if (req.query.name) {
+            name = req.query.name;
+        }
+
+        return await this.personService.read(page, limit, name);
     }
 
 }
--- src/modules/person/person.service.ts
+++ src/modules/person/person.service.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable, Post, UseGuards } from '@nestjs/common';
+import sequelize from 'sequelize';
 import { PERSON_REPOSITORY } from 'src/core/constants';
 import { Person } from './person.entity';
 
@@ -8,5 +9,40 @@ export class PersonService {
         @Inject(PERSON_REPOSITORY) private readonly personRepository: typeof Person
     ) {}
 
-    async create(): Promise<Person> { return Promise.resolve(new Person)}
+    async create(body, userId): Promise<Person> { 
+        return await this.personRepository.create({...body, userId})
+    }
+
+    async read(page?: number, limit?: Number, name?: string) {
+
+        limit = limit || 2;
+        page = page || 1;
+
+        const filter = {};
+        console.log(filter)
+        const offset = Number(limit) * (Number(page) - 1);
+
+        if (name) {
+            Object.assign(filter, { name: {[sequelize.Op.like]:'%' + name + '%' } });
+        }
+
+        const {rows, count} = await this.personRepository.findAndCountAll({ where: filter, attributes: { exclude: ['password', 'deletedAt'] }, order: ['id'], offset, limit: Number(limit)});
+
+        if (rows.length > 0) {
+            const totalPages = Math.ceil(count / Number(limit));
+            return {
+                totalItems: count,
+                rows,
+                actualPage: Number(page),
+                totalPages
+            };
+        } else {
+            return { message: "There are not persons available!" }
+        }
+
+    }
+
+    async findOneByUserId(userId: number) {
+       return await this.personRepository.findOne({ where: { userId } });
+    }
 }
