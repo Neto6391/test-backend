808b85f5192a50ebf2292cf45ca83b357d19ba8e
nettorodrigues <neto6391@gmail.com>
2021-06-03 20:04:38 -0300

    Begin functionality of create Person

--- src/modules/person/person.controller.ts
+++ src/modules/person/person.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/core/guards/jwt-auth.guard';
+import { PersonService } from './person.service';
 
 @Controller('person')
-export class PersonController {}
+export class PersonController {
+    constructor(
+        private readonly personService: PersonService
+    ) {}
+
+    @UseGuards(JwtAuthGuard)
+    @Post('create')
+    async create(@Body() body, @Request() req) {
+        
+    }
+
+}
+
+
--- src/modules/person/person.service.ts
+++ src/modules/person/person.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Post, UseGuards } from '@nestjs/common';
+import { PERSON_REPOSITORY } from 'src/core/constants';
+import { Person } from './person.entity';
 
 @Injectable()
-export class PersonService {}
+export class PersonService {
+    constructor(
+        @Inject(PERSON_REPOSITORY) private readonly personRepository: typeof Person
+    ) {}
+
+    async create(): Promise<Person> { return Promise.resolve(new Person)}
+}
