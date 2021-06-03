5e039e12933c487a1efa573c3345f3a8666c5921
nettorodrigues <neto6391@gmail.com>
2021-06-03 13:57:39 -0300

    refactor user Controller and increment delete route

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Post, UseGuards, Request, Body } from '@nestjs/common';
+import { Controller, Post, UseGuards, Request, Body, Delete, Param, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { DoesUserExist } from 'src/core/guards/doesUserExist.guard';
+import { DoesUserExist } from 'src/core/guards/does-user-exists.guard';
+import { JwtAuthGuard } from 'src/core/guards/jwt-auth.guard';
+import { LocalAuthGuard } from 'src/core/guards/local-auth.guard';
 import { UserDto } from './dto/user.dto';
 import { UserService } from './user.service';
 
@@ -10,16 +12,32 @@ export class UserController {
         private userService: UserService
     ) {}
 
-    @UseGuards(AuthGuard('local'))
+
+    @UseGuards(LocalAuthGuard)
     @Post('authenticate')
-    async login(@Request() req) {
+    async authenticate(@Request() req ) {
         return await this.userService.authenticate(req.user);
+    
     }
 
     @UseGuards(DoesUserExist)
     @Post('create')
-    async signUp(@Body() user: UserDto) {
+    async create(@Body() user: UserDto) {
         return await this.userService.create(user);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Delete(':id')
+    async softDelete(@Param('id') idParam: number, @Request() req) {
+        const { id: userIdAuth, isAdmin: isUserAdminAuth } = req.user
+        const deleted = await this.userService.softDelete(idParam, userIdAuth, isUserAdminAuth);
+
+        if (deleted === null) {
+            throw new UnauthorizedException('You are not authorized to perfom the operation');
+        } else if(deleted === 0) {
+            throw new NotFoundException('This User doesn\'t exist');
+        }
+        return 'Successfully deleted';
     }
 
 }
