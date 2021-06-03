404fa1fda23773480e95f034cc8308d1a45f3431
nettorodrigues <neto6391@gmail.com>
2021-06-03 13:56:35 -0300

    refactor User Model and some incrementing field for isAdmin

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -1,17 +1,36 @@
-import { Column, DataType, Model, Table } from "sequelize-typescript";
+import { AutoIncrement, Column, DataType, Default, Model, PrimaryKey, Table, Unique } from "sequelize-typescript";
 
-@Table
+@Table({
+    paranoid: true
+})
 export class User extends Model {
+    @PrimaryKey
+    @AutoIncrement
+    @Unique
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false,
+    })
+    id: number;
+
+    @Unique
     @Column({
         type: DataType.STRING,
         allowNull: false,
     })
     email: string;
-
+   
     @Column({
         type: DataType.STRING,
         allowNull: false
     })
     password: string;
 
+
+    @Default(false)
+    @Column({
+        type: DataType.BOOLEAN,
+        allowNull: false
+    })
+    isAdmin: boolean;
 }
