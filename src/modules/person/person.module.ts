74642f19652d0e638dc60368eb8fe7d9363ccb46
nettorodrigues <neto6391@gmail.com>
2021-06-03 20:04:10 -0300

    import Person Repository from Person Module

--- src/modules/person/person.module.ts
+++ src/modules/person/person.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { PersonService } from './person.service';
 import { PersonController } from './person.controller';
+import { AuthModule } from '../auth/auth.module';
+import { personsProviders } from './person.providers';
 
 @Module({
-  providers: [PersonService],
+  imports: [ forwardRef(()=> AuthModule)],
+  providers: [PersonService, ...personsProviders],
   controllers: [PersonController]
 })
 export class PersonModule {}
