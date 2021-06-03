494cf4983b8cceef8f67d1b17ff4c019d94034d8
nettorodrigues <neto6391@gmail.com>
2021-06-03 01:21:21 -0300

    import repository for Model User

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { usersProviders } from './users.providers';
 
 @Module({
-  providers: [UserService],
+  providers: [UserService, ...usersProviders],
   controllers: [UserController]
 })
 export class UserModule {}
