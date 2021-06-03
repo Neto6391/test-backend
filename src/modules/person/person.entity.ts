e3690de8e16e06190329f6a8f22e1555ca6370fe
nettorodrigues <neto6391@gmail.com>
2021-06-03 20:00:59 -0300

    create first version of Model Person

---
+++ src/modules/person/person.entity.ts
@@ -0,0 +1,40 @@
+import { AutoIncrement, BelongsTo, Column, DataType, ForeignKey, HasOne, Model, PrimaryKey, Table, Unique } from "sequelize-typescript";
+import { User } from "../user/user.entity";
+
+@Table
+export class Person extends Model {
+    @PrimaryKey
+    @Unique
+    @AutoIncrement
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false
+    })
+    id: number;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    name: string;
+
+    @Column({
+        type: DataType.ENUM,
+        values: ['male', 'female'],
+        allowNull: false
+    })
+    gender: string;
+
+    @Column({
+        type: DataType.DATE,
+        allowNull: false
+    })
+    birthday: Date;
+
+    @ForeignKey(() => User)
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false,
+    })
+    userId: number;
+}
