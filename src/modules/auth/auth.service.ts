9734ad858df5471a878b913b34c8a2f2c2dae0b6
nettorodrigues <neto6391@gmail.com>
2021-06-03 03:11:56 -0300

    create a service for auth module

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,45 @@
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { UserService } from '../user/user.service';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        @Inject(forwardRef(() => UserService)) private readonly userService: UserService,
+        // private readonly userService: UserService,
+        private readonly jwtService: JwtService
+    ) { }
+
+    async validateUser(email: string, pass: string) {
+        const user = await this.userService.findOneByEmail(email);
+        
+        if (!user) {
+            return null;
+        }
+
+        const match = await this.comparePassword(pass, user.password);
+        if (!match) {
+            return null;
+        }
+
+        const { password, ...result } = user['dataValues'];
+        return result;
+    }
+
+    public async login(user) {
+        const token = await this.generateToken(user);
+        return { user, token };
+    }
+
+    public async generateToken(user) {
+        const token = await this.jwtService.signAsync(user);
+        return token;
+    }
+    
+
+    private async comparePassword(enteredPassword, dbPassword) {
+        const match = await bcrypt.compare(enteredPassword, dbPassword);
+        return match;
+    }
+}
