31138262b8a4aa5c48c273a0ef65d85d1526b0d4
nettorodrigues <neto6391@gmail.com>
2021-06-03 03:15:34 -0300

    import auth for use in User module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { usersProviders } from './users.providers';
+import { AuthModule } from '../auth/auth.module';
+import { AuthService } from '../auth/auth.service';
 
 @Module({
-  providers: [UserService, ...usersProviders],
-  controllers: [UserController]
+  imports: [ forwardRef(()=> AuthModule)],
+  providers: [
+    ...usersProviders,
+    UserService
+  ],
+  controllers: [UserController],
+  exports: [UserService]
 })
 export class UserModule {}
